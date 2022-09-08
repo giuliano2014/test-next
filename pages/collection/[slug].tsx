83b340ff52437431e03d07e11b4669deeaf67d18
Giuliano Furgol <giulianofurgol@hotmail.com>
2022-09-08 17:57:42 +0200

    Add Apollo to test GraphQL Queries

--- pages/collection/[slug].tsx
+++ pages/collection/[slug].tsx
@@ -1,45 +1,93 @@
+import { gql } from '@apollo/client'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Head from 'next/head'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import client from '../../apollo-client'
 
-const SingleItem = () => {
-  const { locale, locales, query } = useRouter()
-  const { slug } = query
+const SingleItem = ({ product }: any) => {
+  const router = useRouter()
+  const { locale } = router
   const { t } = useTranslation('common')
+ 
+  // Display loading until `getStaticProps()` finishes running, and populates the props.
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
 
   return (
     <>
       <Head>
-        <title key="metaTitle">{slug}</title>
-        <meta key="metaDescription" name="description" content={`${slug} description`} />
+        <title key="metaTitle">{product.name}</title>
+        <meta key="metaDescription" name="description" content={product.description} />
       </Head>
       <Link href="/collection" locale={locale}>
         <a>{t('back')}</a>
       </Link>
-      <h1>{slug}</h1>
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
     </>
   )
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async ({ locales }: any) => {
+  const [ fr, en ] = locales
+
+  const { data } = await client.query({
+    query: gql`
+      query Products($localesFr: Locale!, $localesEn: Locale!) {
+        ${fr}: products(locales: [$localesFr]) {
+          slug
+        }
+        ${en}: products(locales: [$localesEn]) {
+          slug
+        }
+      },
+    `,
+    variables: {
+      localesFr: fr,
+      localesEn: en,
+    },
+  })
+
+  const pathsFr = data[fr].map((product: any) => ({ params: { slug: product.slug }, locale: fr }))
+  const pathsEn =  data[en].map((product: any) => ({ params: { slug: product.slug }, locale: en }))
+  const paths = [...pathsFr, ...pathsEn]
+
   return {
-    paths: [
-      // if no `locale` is provided only the defaultLocale will be generated
-      { params: { slug: 'my-first-item' }, locale: 'en' },
-      { params: { slug: 'my-second-item' }, locale: 'en' },
-      { params: { slug: 'my-first-item' }, locale: 'fr' },
-      { params: { slug: 'my-second-item' }, locale: 'fr' },
-    ],
+    paths,
     fallback: true,
   }
 }
 
-export const getStaticProps = async ({ locale }: any) => ({
-  props: {
-    ...await serverSideTranslations(locale, ['common']),
-  },
-})
+export const getStaticProps: GetStaticProps = async ({ locale, params }: any) => {
+  const { slug } = params
+
+  const { data } = await client.query({
+    query: gql`
+      query GetProductByLocaleAndSlug($locales: Locale!, $slug: String!) {
+        products(locales: [$locales], where: {slug: $slug}) {
+          name
+          description
+        }
+      },
+    `,
+    variables: {
+      locales: locale,
+      slug: slug,
+    },
+  })
+
+  const { products } = data
+  
+  return {
+    props: {
+      product: products[0],
+      ...await serverSideTranslations(locale, ['common']),
+    },
+  }
+}
 
 export default SingleItem
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Head from 'next/head'
 
-const Home: NextPage = () => {
+import { gql } from "@apollo/client"
+import client from "../apollo-client"
+
+const Home: NextPage = ({ home }: any) => {
   const { t } = useTranslation('common')
+  // const home = homes[0]
 
   return (
     <>
@@ -12,15 +16,65 @@ const Home: NextPage = () => {
         <title key="metaTitle">Home</title>
         <meta key="metaDescription" name="description" content="Home description" />
       </Head>
-      <h1>{t('navigation.home')}</h1>
+      <h1>Traduction : {t('navigation.home')}</h1>
+      <h1>{home.title}</h1>
+      <p>{home.description}</p>
+      <p>{home.slug}</p>
     </>
   )
 }
 
-export const getStaticProps = async ({ locale }: any) => ({
-  props: {
-    ...await serverSideTranslations(locale, ['common']),
-  },
-})
+export const getStaticProps: GetStaticProps = async ({ locale }: any) => {
+  const { data } = await client.query({
+    query: gql`
+      query Home($id: ID!, $locales: Locale!) {
+        home(where: { id: $id }, locales: [$locales]) {
+          description
+          slug
+          title
+        }
+      },
+    `,
+    variables: {
+      id: "cl7t1xsvsfbvn0buqeiiltvdh",
+      locales: locale
+    },
+  })
+
+  const { home } = data
+
+  return {
+    props: {
+      home,
+      ...await serverSideTranslations(locale, ['common']),
+    },
+  }
+}
+
+// export const getStaticProps: GetStaticProps = async ({ locale }: any) => {
+//   const { data } = await client.query({
+//     query: gql`
+//       query Homes($locales: Locale!) {
+//         homes(locales: [$locales]) {
+//           description
+//           slug
+//           title
+//         }
+//       }
+//     `,
+//     variables: {
+//       locales: locale
+//     },
+//   })
+
+//   const { homes } = data
+
+//   return {
+//     props: {
+//       homes,
+//       ...await serverSideTranslations(locale, ['common']),
+//     },
+//   }
+// }
 
 export default Home
