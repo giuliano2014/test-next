8fe16dffce78dc93cff6998925128829508719bc
Giuliano Furgol <giulianofurgol@hotmail.com>
2022-09-13 16:54:37 +0200

    Add apollo with another technic

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,33 @@ import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Head from 'next/head'
 
+import { gql, useQuery } from "@apollo/client"
+
+const HOME_QUERY = gql`
+  query Home($id: ID!, $locales: Locale!) {
+    home(where: { id: $id }, locales: [$locales]) {
+      description
+      slug
+      title
+    }
+  }
+`;
+
 const Home: NextPage = () => {
   const { t } = useTranslation('common')
+  let { loading, data, error } = useQuery(HOME_QUERY, {
+    variables: {
+      id: 'cl7t1xsvsfbvn0buqeiiltvdh',
+      locales: 'fr'
+    },
+  });
+
+  console.log('loading', loading)
+  console.log('data', data)
+  console.log('error', error)
+
+  if (loading) return <div>Loading...</div>
+  if (error) return <div>Oops, something went wrong {error.message}</div>
 
   return (
     <>
@@ -12,7 +37,10 @@ const Home: NextPage = () => {
         <title key="metaTitle">Home</title>
         <meta key="metaDescription" name="description" content="Home description" />
       </Head>
-      <h1>{t('navigation.home')}</h1>
+      <h1>{t('navigation.home')} (from json traduction)</h1>
+      <h1>{data.home.title}</h1>
+      <p>{data.home.description}</p>
+      <p>{data.home.slug}</p>
     </>
   )
 }
